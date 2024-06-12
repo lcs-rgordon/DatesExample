b006c1e3f5a4139676475a32781a2a28823ce934
Russell Gordon <rgordon@lcs.on.ca>
2024-06-12 18:03:14 -0400

    Added package for Supabase and organized project structure for MVVM.

--- DatesExample/DatesExampleApp.swift
+++ DatesExample/DatesExampleApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct DatesExampleApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            DatesListView()
         }
     }
 }
---
+++ DatesExample/Views/DatesListView.swift
@@ -0,0 +1,18 @@
+//
+//  DatesListView.swift
+//  DatesExample
+//
+//  Created by Russell Gordon on 2024-06-12.
+//
+
+import SwiftUI
+
+struct DatesListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DatesListView()
+}
