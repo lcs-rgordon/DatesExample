4917329fddf3634573a899626ca3270d7ded55c9
Russell Gordon <rgordon@lcs.on.ca>
2024-06-12 11:07:36 -0400

    Got database helper started.

---
+++ DatesExample/Helpers/Supabase.swift
@@ -0,0 +1,13 @@
+//
+//  Supabase.swift
+//  DatesExample
+//
+//  Created by Russell Gordon on 2024-06-12.
+//
+
+import Foundation
+
+let supabase = SupabaseClient(
+  supabaseURL: URL(string: "https://wroubjpjqmjrfdimcbgs.supabase.co")!,
+  supabaseKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Indyb3VianBqcW1qcmZkaW1jYmdzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTgyMDM4MDEsImV4cCI6MjAzMzc3OTgwMX0.aT3C8uMQgah73SIjJKTesgnDDXQIWCOqJK6AB9JThJ8"
+)
