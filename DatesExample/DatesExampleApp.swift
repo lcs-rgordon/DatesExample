5a47e8565c4fbae9c8741a06791d2bbcac95d2f7
Russell Gordon <rgordon@lcs.on.ca>
2024-06-12 11:05:33 -0400

    Initial Commit

---
+++ DatesExample/DatesExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  DatesExampleApp.swift
+//  DatesExample
+//
+//  Created by Russell Gordon on 2024-06-12.
+//
+
+import SwiftUI
+
+@main
+struct DatesExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
